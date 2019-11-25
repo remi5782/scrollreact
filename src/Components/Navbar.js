2eaab3e766e9c4113bae1293e7985ccd20e5d4a0
Anbarasan parimalaselvan <anbuanbu@192-168-1-103.tpgi.com.au>
2019-11-26 00:51:41 +1100

    initial commit

---
+++ src/Components/Navbar.js
@@ -0,0 +1,99 @@
+import React, { Component } from "react";
+import logo from "../logo.svg";
+import { Link, animateScroll as scroll } from "react-scroll";
+
+export default class Navbar extends Component {
+  scrollToTop = () => {
+    scroll.scrollToTop();
+  };
+
+  handleSetActive = (to)=>console.log(to);
+  
+  render() {
+    const {setActiveSection} =this.props;
+    return (
+      <nav className="nav" id="navbar">
+        <div className="nav-content">
+          <img
+            src={logo}
+            className="nav-logo"
+            alt="Logo"
+            onClick={this.scrollToTop}
+          />
+          <ul className="nav-items">
+            <li className="nav-item">
+              <Link
+                activeClass="active"
+                to="section1"
+                spy={true}
+                smooth={true}
+                offset={-70}
+                duration={500}
+                onClick={()=>setActiveSection('section1')}
+                onSetActive={(to)=> setActiveSection(to)}
+              >
+                Section 1
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                activeClass="active"
+                to="section2"
+                spy={true}
+                smooth={true}
+                offset={-70}
+                duration={500}
+                onClick={()=>setActiveSection('section2')}
+                onSetActive={(to)=> setActiveSection(to)}
+              >
+                Section 2
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                activeClass="active"
+                to="section3"
+                spy={true}
+                smooth={true}
+                offset={-70}
+                duration={500}
+                onClick={()=>setActiveSection('section3')}
+                onSetActive={(to)=> setActiveSection(to)}
+              >
+                Section 3
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                activeClass="active"
+                to="section4"
+                spy={true}
+                smooth={true}
+                offset={-70}
+                duration={500}
+                onClick={()=>setActiveSection('section4')}
+                onSetActive={(to)=> setActiveSection(to)}
+              >
+                Section 4
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                activeClass="active"
+                to="section5"
+                spy={true}
+                smooth={true}
+                offset={-70}
+                duration={500}
+                onClick={()=>setActiveSection('section5')}
+                onSetActive={(to)=> setActiveSection(to)}
+              >
+                Section 5
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    );
+  }
+}
---
+++ src/Components/Section.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Section({ title, subtitle, dark, id, activeContent }) {
+  return (
+    <div className={"section" + (dark ? " section-dark" : "")}>
+      <div className={"section-content" + (activeContent ? " active": "")} id={id}>
+        <h1>{title}</h1>
+        <p>{subtitle}</p>
+      </div>
+    </div>
+  );
+}
