28d7e477e52c98ed25ee7efc63d0511bf15a28cb
Anbarasan parimalaselvan <anbuanbu@192-168-1-103.tpgi.com.au>
2019-12-10 10:17:43 +1100

    latest_commit

--- src/Components/GeneralSection.js
+++ src/Components/GeneralSection.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import {Typography} from '@material-ui/core';
+import { loadCSS } from 'fg-loadcss';
 // import GridList from './Utils/GridList';
+
 export default function GeneralSection({ title, subtitle, dark, id, activeContent }) {
+    React.useEffect(() => {
+        loadCSS(
+          'https://use.fontawesome.com/releases/v5.1.0/css/all.css',
+          document.querySelector('#font-awesome-css'),
+        );
+      }, []);
     return (
         <div className={"section" + (dark ? " section-dark" : "")}>
             <div className={"section-content" + (activeContent ? " active" : "")} id={id}>
-                <Typography variant="title" component="h1">
-                    I'm Remy and am a Front-End Web Developer
+                <Typography variant="h4" component="h5">
+                    I'm Remy and am a Front-End Web Developer and technology enthusiast who loves to do InnovativE works
                 </Typography>
                 {/* <h1>{title}</h1> */}
                 {/* <GridList/> */}
--- src/Components/Section.js
+++ src/Components/Section.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Button from "@material-ui/core/Button";
+import Link from "@material-ui/core/Link";
 export default function Section({ title, subtitle, dark, id, activeContent }) {
 const handleClick= ()=>{
   window.open('/CV_Latest.docx')
@@ -12,6 +13,12 @@ const handleClick= ()=>{
         <Button onClick={()=>handleClick()} variant="contained" color="secondary">
   Download CV
 </Button>
+<Link style={{display: 'block',marginTop: '2rem'}} href={"https://github.com/remi5782"} variant="contained" color="secondary">
+  <i>My GitHub Repo</i>
+</Link>
+<Link style={{display: 'block',marginTop: '2rem'}} href={"https://www.linkedin.com/in/remi-devaraju-09770420/"} variant="contained" color="secondary">
+  <i>My LinkedIn Page</i>
+</Link>
       </div>
     </div>
   );
